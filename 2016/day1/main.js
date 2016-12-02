9cd473eaa068e783ea55b79f90ee72cc1d70cdc4
Alex Wilson <a@ax.gy>
2016-12-02 01:24:33 +0000

    Solution to part 1

--- 2016/day1/main.js
+++ 2016/day1/main.js
@@ -2,10 +2,56 @@ const path = require('path');
 const fs = require('fs');
 
 // Load and parse the input.
-const loadFile = _ => fs.readFileSync(path.resolve(__dirname, "./input.txt")).toString().split(', ');
+const loadFile = _ => fs.readFileSync(path.resolve(__dirname, "./input.txt")).toString().split(', ')
 
-function main() {
-    console.log(loadFile().join('-'));
+function calculateDistance(data) {
+
+    // 360 degrees in a circle.
+    const circle = 360;
+
+    // path finder struct.
+    const pathFinder = {
+
+        // Distance walked in each given direction.
+        delta: {
+            0: 0,
+            90: 0,
+            180: 0, 
+            270: 0
+        },
+
+        // Direction facing.
+        facing: 0
+    }
+
+    // Ensure that our directions wrap around a circle.
+    const wrapDirection = newDirection => (newDirection < 0) ? circle + newDirection : newDirection % circle
+
+    // From a given direction, apply an instruction to receive a new direction.
+    const newDirection = (currentDirection, instruction) => wrapDirection(currentDirection + newDirectionDelta(instruction))
+    const newDirectionDelta = directionInstruction => directionInstruction === 'L' ? -90 : 90
+
+    // Walk over input with pathfinder, to reduce to a delta.
+    const delta = data.reduce((pathFinder, instruction) => {
+
+        // Direction faced is stateful, we need to preserve this in order to turn.
+        pathFinder.facing = newDirection(pathFinder.facing, instruction[0])
+
+        // Delta is incremental, we add on the current distance.
+        pathFinder.delta[pathFinder.facing] += parseInt(instruction.slice(1))
+        return pathFinder
+
+    }, pathFinder).delta
+
+    // Subtract distance travelled South from North, and East from West.
+    const totalDistance = Math.abs(delta[0] - delta[180]) + Math.abs(delta[90] - delta[270])
+    return totalDistance
 }
 
-main();
+function main() {
+    console.info(`Total distance travelled: ${calculateDistance(loadFile())}`)
+}
+
+module.exports = { calculateDistance }
+
+main()
---
+++ 2016/day1/test.js
@@ -0,0 +1,9 @@
+const main = require('./main.js');
+const assert = require('assert');
+
+assert.strictEqual(main.calculateDistance(['R2', 'L3']), 5)
+assert.strictEqual(main.calculateDistance(['R2', 'R2', 'R2']), 2)
+assert.strictEqual(main.calculateDistance(['R5', 'L5', 'R5', 'R3']), 12)
+
+assert.strictEqual(main.calculateDistance(['R1', 'R1', 'R1', 'R1']), 0)
+assert.strictEqual(main.calculateDistance(['R1', 'R1', 'R1', 'R1', 'L2', 'R250']), 252)
