ebe973e8a98bcbd2670a3c1a2920d70609105b8b
Alex Wilson <a@ax.gy>
2016-12-13 23:02:25 +0000

    Part one of Day 6

---
+++ 2016/day6/main.js
@@ -0,0 +1,41 @@
+const loadFile = require('../../util/js/loadFile').default
+const os = require('os')
+
+// Load and parse the input.
+const loadData = _ => loadFile().split(os.EOL)
+
+function rowsToColumns(data) {
+    const totalColumns = data[0].length
+
+    // Construct an empty mutable array to push values into.
+    const emptyArray = Array.from([...Array(totalColumns).keys()]).map(_ => [])
+
+    // Reduce list of rows, so we can push into collection of columns.
+    return data.reduce((previous, current) => {
+        current.split('').forEach((value, index) => {
+            previous[index].push(value)
+        })
+        return previous
+    }, emptyArray)
+}
+
+// Decode by finding the most common character.
+function decode(data) {
+
+    // Map over columns and return most frequent character.
+    return data.map(value => {
+        const histogram = {}
+        value.forEach(letter => histogram[letter] = (histogram[letter] || 0) + 1)
+        return Object.keys(histogram).sort((a, b) => histogram[a] - histogram[b]).pop()
+    }).join('')
+}
+
+// Main function.
+function main() {
+    const data = loadData()
+    console.info(`Answer to part 1, message is: ${decode(rowsToColumns(data))}`)
+}
+
+main()
+
+module.exports = { decode, rowsToColumns }
---
+++ 2016/day6/test.js
@@ -0,0 +1,23 @@
+const main = require('./main.js')
+const assert = require('assert')
+const os = require('os')
+
+const easter = 
+`eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar`.split(os.EOL)
+
+assert.strictEqual(main.decode(main.rowsToColumns(easter)), 'easter');
